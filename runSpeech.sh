0e20c853b9e8db2990d7025e8f75c028b20d4aec
Adam <adrtmrber@live.com>
2021-04-10 12:06:20 -0400

    first update

---
+++ runSpeech.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+
+# ls ~/winhome/Documents/'Sound recordings'/"Recording.m4a" 
+
+
+mv ~/winhome/Documents/'Sound recordings'/Recording.m4a ~/winhome/Documents/'Sound recordings'/"Recording$1.m4a"
+
+cp ~/winhome/Documents/'Sound recordings'/Recording$1.m4a recordings/
+
+echo 'y' | ffmpeg -i "recordings/Recording$1.m4a" Recording.wav
+
+./speech
+
+explorer.exe response.wav
