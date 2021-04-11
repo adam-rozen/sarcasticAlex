adc0fdbb246ea8d67e276947a80543082cbe4e76
MessMaker236 <bm121512@gmail.com>
2021-04-11 06:45:54 -0400

    Update functions.py

--- functions.py
+++ functions.py
@@ -72,3 +72,30 @@ def getText(subscription_key, speech):
 
     with open(speech, 'rb') as in_file:
         return requests.post(url=stt_eastus_url, data=in_file, headers=headers)
+
+def setupLEDs():
+    
+    import RPi.GPIO as GPIO   # Import the GPIO library.
+    import time               # Import time library
+
+    GPIO.setmode(GPIO.BOARD)  # Set Pi to use pin number when referencing GPIO pins.
+
+    GPIO.setup(12, GPIO.OUT)  # Set GPIO pin 12 to output mode.
+    GPIO.setup(18, GPIO.OUT)  # Set GPIO pin 12 to output mode.
+    GPIO.setup(24, GPIO.OUT)
+
+    redpwm = GPIO.PWM(12, 100)   # Initialize PWM on pwmPin 100Hz frequency
+    greenpwm = GPIO.PWM(18, 100)   
+    bluepwm = GPIO.PWM(24, 100)    
+
+    redpwm.start(0)                      # Start PWM with 0% duty cycle
+    greenpwm.start(0)
+    bluepwm.start(0)
+
+def displayColor(r, g, b): #Accepts RGB values on 256 scale, MUST run setupLEDs() first
+    
+    redpwm.ChangeDutyCycle(r * 256/100)
+    greenpwm.ChangeDutyCycle(g * 256/100)
+    bluepwm.ChangeDutyCycle(b * 256/100)
+    
+    
